28233177a2a154628e2961a8a94aed975ab6d91b
monikaaat <monika.tamelyte@mif.stud.vu.lt>
2025-09-20 18:29:19 +0300

    Add basic hashing algorithm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "include/lib.h"
 
 int main(){
+    /*char raide;
+    std::cin >> raide;
+    int verte = int(raide);
+    std::cout << verte << std::endl;
+ //   int verte;
+   // std::cin >> verte;
+    char pakeistaRaide = (char)verte;
+    std::cout << pakeistaRaide;*/
+    
     std::string input;
     std::cout << "Kokį tekstą norite hashuoti?" << std::endl;
     std::getline(std::cin, input);
