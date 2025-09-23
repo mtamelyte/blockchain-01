82f8b94046b59f6185970fb00922fec6069587df
monikaaat <monika.tamelyte@mif.stud.vu.lt>
2025-09-23 14:48:40 +0300

    Change it to care about the order of input letters

--- src/bitwiseHash.cpp
+++ src/bitwiseHash.cpp
@@ -1,10 +1,8 @@
 #include "../include/lib.h"
 
-int hashLength = 64;
-
 std::string hash(std::string input)
 {
-    int hash[64]{15};
+    int hash[64]{0};
     std::stringstream finalHash;
     for (char inputChar : input)
     {
@@ -13,47 +11,18 @@ std::string hash(std::string input)
         std::uniform_int_distribution<int> number(0, 15);
         for (int i = 0; i < 64; i++)
         {
-            
+            hash[i] = abs(hash[i] - number(change));
         }
-    }
-
-    finalHash << std::hex << value;
-    return finalHash.str();
-}
-
-/*
-{
-    int hash[64]{0};
-    long long totalValue = 0;
-    std::stringstream finalHash;
-    for (int a = 0; a < input.length(); a++)
-    {
-        int utfVal = (int)input[a];
-        totalValue += utfVal;
-
-        std::bitset<8> binVal(input[a]);
-        for (int i = 0; i < 8; i++)
+        int temp = hash[0];
+        for (int i = 0; i < 64; i++)
         {
-            if (binVal[i])
-            {
-                for (int j = i; j < 64; j += 8)
-                {
-                    hash[j]++;
-                }
-            }
+            hash[i] = hash[i + 1];
         }
+        hash[63] = temp;
     }
-
-    std::mt19937 change(totalValue);
-    std::uniform_int_distribution<int> number(0, 15);
     for (int i = 0; i < 64; i++)
     {
-        int value = number(change);
-        for (int j = 0; j < hash[i]; j++)
-        {
-            value = number(change);
-        }
-        finalHash << std::hex << value;
+        finalHash << std::hex << hash[i];
     }
     return finalHash.str();
-}*/
+}
